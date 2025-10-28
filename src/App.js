73d41393543c7aab8c7e2267bc7b99f50faf79a5
christianzwilly <139091557+christianzwilly@users.noreply.github.com>
2025-10-28 09:22:02 +0700

    Add App component to render onboarding flow

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import OcbcThemedInvestmentOnboardingFlow from "./ocbc_themed_investment_onboarding_flow_web_mobile_toggle";
+
+function App() {
+  return <OcbcThemedInvestmentOnboardingFlow />;
+}
+
+export default App;
